3aeff93e8cde49290f8af990f5f48b0381ea79e1
Andres Ladino <aladinoster@gmail.com>
2019-06-29 08:53:59 +0200

    Prototyping our first functionality towards MVP

--- circle.py
+++ circle.py
@@ -3,5 +3,12 @@
 
 """
 
+
 class Circle(object):  # A new style class
     "An advanced circle analytic toolkit"
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2.0
