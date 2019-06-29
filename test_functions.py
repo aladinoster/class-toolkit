7f9e940d10dc02806d37c2cf4cd05c10f2bca5f4
Andres Ladino <aladinoster@gmail.com>
2019-06-29 09:55:31 +0200

    Adding new function + use case

--- test_functions.py
+++ test_functions.py
@@ -23,8 +23,21 @@ class TestRandomCircle(unittest.TestCase):
         radius = [random() for _ in range(n)]
         circles = [Circle(r) for r in radius]
         print(f"The average area of {n} random circles is")
-        avg_area = sum([c.area() for c in circles])/n
+        avg_area = sum([c.area() for c in circles]) / n
         print(f"is {avg_area:.1f}")
-        avg_area_calculated = sum([math.pi * r ** 2 for r in radius])/n
+        avg_area_calculated = sum([math.pi * r ** 2 for r in radius]) / n
         self.assertEqual(avg_area, avg_area_calculated)
 
+
+
+class TestExposureParameters(unittest.TestCase):
+    def test_modify_radius_after_creation(self):
+        cuts = [0.1, 0.7, 0.8]
+        circles = [Circle(r) for r in cuts]
+        for c in circles:
+            print(f"A circle with a radius {c.radius}")
+            print(f"has a perimeter of {c.perimeter()}")
+            print(f"and a cold area of {c.area()}")
+            c.radius *= 1.1
+            print(f"and a warm area of {c.area()}")
+            self.assertEqual(c.area(), math.pi * c.radius ** 2)
