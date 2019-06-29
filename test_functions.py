cc75e5ec2bc43f1228384cefc31c7c81b43d43d7
Andres Ladino <aladinoster@gmail.com>
2019-06-29 09:38:02 +0200

    Adding test for academic client

--- test_functions.py
+++ test_functions.py
@@ -2,6 +2,7 @@ import unittest
 
 from circle import Circle
 import math
+from random import random, seed
 
 
 class TestCircle(unittest.TestCase):
@@ -13,3 +14,16 @@ class TestCircle(unittest.TestCase):
         area_calculated = [math.pi * r ** 2 for r in radius]
         self.assertEqual(area, area_calculated)
 
+
+class TestRandomCircle(unittest.TestCase):
+    def test_creation_n_circles(self):
+        seed(8675309)
+        print(f"Using Circuituous(tm) version {Circle.version}")
+        n = 10
+        radius = [random() for _ in range(n)]
+        circles = [Circle(r) for r in radius]
+        print(f"The average area of {n} random circles is")
+        avg_area = sum([c.area() for c in circles])/n
+        print(f"is {avg_area:.1f}")
+        avg_area_calculated = sum([math.pi * r ** 2 for r in radius])/n
+        self.assertEqual(avg_area, avg_area_calculated)
